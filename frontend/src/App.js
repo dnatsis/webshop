6635b812a6f549284e140d016eec2d426d3e24b5
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-03-10 10:12:46 -0500

    Added HomeScreen and Rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to my Web Shop!</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
