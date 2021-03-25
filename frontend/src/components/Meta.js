5ec264b2841294c79f85b20f886d70c0e3e5a69c
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-03-25 13:32:43 -0400

    added custom page titles and meta with Helmet

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: 'Welcome To WebShop',
+  description: 'We sell the finest mediterranean products',
+  keywords: 'mediterranean, buy mediterranean foods',
+};
+
+export default Meta;
