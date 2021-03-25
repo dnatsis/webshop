f98626e6d56eafaedad3217b2d97f86c08ec6c1d
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-03-25 10:19:21 -0400

    Added SearchBox component and implemented Search functionality in the backend and on the HomeScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -17,10 +17,7 @@ import {
   listProductDetails,
   createProductReview,
 } from '../actions/productActions';
-import {
-  PRODUCT_CREATE_REVIEW_FAIL,
-  PRODUCT_CREATE_REVIEW_RESET,
-} from '../constants/productConstants';
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants';
 
 const ProductScreen = ({ history, match }) => {
   const [qty, setQty] = useState(1);
