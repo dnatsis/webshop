88781226df2513ec83e1a0ab725b64db6e195caa
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-03-25 12:55:10 -0400

    Added getTopProducts controller and route in the backend, created ProductCarousel component in the frontend and implemented it in the HomeScreen

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -109,7 +109,7 @@ const updateProduct = asyncHandler(async (req, res) => {
 
 // @desc    Create new review
 // @route   POST /api/products/:id/reviews
-// @access    private
+// @access    Private
 const createProductReview = asyncHandler(async (req, res) => {
   const { rating, comment } = req.body;
 
@@ -148,6 +148,15 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Create top rated products
+// @route   GET /api/products/top
+// @access    Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
 export {
   getProducts,
   getProductById,
@@ -155,4 +164,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
