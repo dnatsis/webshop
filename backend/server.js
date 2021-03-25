11e1c8539d0af969e74c92be756b05a94bbefa02
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-03-24 22:16:01 -0400

    added createProductReview controller and route in the backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -16,6 +17,10 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => {
   res.send('API is running...');
